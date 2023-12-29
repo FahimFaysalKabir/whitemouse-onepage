f28f7e28138c7fbd6f5c7eda314e4ab893f18035
Fahim Faysal Kabir <githubbadhon@gmail.com>
2023-12-30 04:43:09 +0600

    30.12.2023

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+<!-------Download End------->
+
+<footer class="footer-copy">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <p>2018 &copy; Applight. Website Designed by <a href="http://w3Template.com" target="_blank" rel="dofollow">W3 Template</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer();?>
+</body>
+</html>
